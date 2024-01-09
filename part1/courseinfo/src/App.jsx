d435c3d87a7eb3037d35f791c184725ae5104fac
Yusuf Celil Alak <yusuf.celil.alak@gmail.com>
2024-01-09 22:51:12 +0300

    Part1 -> Exercise 1.4 is added

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,27 +3,33 @@ import Header from "./Header"
 import Total from "./Total"
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
+
+  const total = parts.reduce((n, part)=>{
+    return n + part.exercises;
+  },0);
 
   return (
     <div>
       <Header courseName={course}/>
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts} />
+      <Total total={total}/>
     </div>
-  )
+  );
 }
 
 export default App;
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,9 +3,11 @@ import Part from "./Part";
 const Content = (props) => {
     return (
         <div>
-            <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
-            <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
-            <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+            {
+                props.parts.map((part)=>{
+                    return <Part name={part.name} exercises={part.exercises}/>
+                })
+            }
         </div>
     );
 }
