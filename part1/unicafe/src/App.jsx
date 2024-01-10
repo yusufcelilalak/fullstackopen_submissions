b0354ba3844f20238e145de8c135e374549a894b
Yusuf Celil Alak <yusuf.celil.alak@gmail.com>
2024-01-10 21:42:06 +0300

    Part1 -> Exercise 1.7 is added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -28,6 +28,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad ) === 0 ? 0 : (good - bad) / (good + neutral + bad)}</p>
+      <p>positive {(good + neutral + bad ) === 0 ? 0 : (good * 100) / (good + neutral + bad)} %</p>
       
     </div>
   )
