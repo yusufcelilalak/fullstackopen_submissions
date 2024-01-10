cf09e6c606a321427dd559241eb7d06902c4e083
Yusuf Celil Alak <yusuf.celil.alak@gmail.com>
2024-01-10 21:49:17 +0300

    Part1 -> Exercise 1.9 is added

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -2,12 +2,19 @@ const Statistics = ({good, neutral, bad}) => {
     return (
         <div>
             <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {good + neutral + bad}</p>
-            <p>average {(good - bad ) === 0 ? 0 : (good - bad) / (good + neutral + bad)}</p>
-            <p>positive {(good + neutral + bad ) === 0 ? 0 : (good * 100) / (good + neutral + bad)} %</p>
+            {
+                (good + neutral + bad) === 0 ?
+                    <p>No feedback given</p>
+                    :
+                    <>
+                        <p>good {good}</p>
+                        <p>neutral {neutral}</p>
+                        <p>bad {bad}</p>
+                        <p>all {good + neutral + bad}</p>
+                        <p>average {(good - bad) / (good + neutral + bad)}</p>
+                        <p>positive {(good * 100) / (good + neutral + bad)} %</p>
+                    </>
+            }
         </div>
     )
 }
