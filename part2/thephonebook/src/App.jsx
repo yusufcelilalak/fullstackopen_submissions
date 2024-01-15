678d5673fccb2486d94ec7b6e534ba9efb33c55a
Yusuf Celil Alak <yusuf.celil.alak@gmail.com>
2024-01-16 00:05:19 +0300

    Part2 -> Exercise 2.12 is added

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -46,12 +46,15 @@ const App = () => {
       const newPerson = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1
       }
   
-      setPersons(persons.concat(newPerson));
-      setNewName('');
-      setNewNumber('');
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then((response)=>{
+          setPersons(persons.concat(response.data));
+          setNewName('');
+          setNewNumber('');
+        })
     }
   }
 
